f141b7b77e3bbc7e5c201ef457defa0a58e76990
cesytar <cesytarr@gmail.com>
2025-04-11 00:52:52 +0200

    check8

---
+++ ejercicios.js
@@ -0,0 +1,20 @@
+//Crear un bucle for
+let miLista = ['velma', 'exploradora', 'jane', 'jhon', 'harry'];
+
+for (let i = 0; i < miLista.length; i++) {
+
+ console.log(miLista[i]);
+  }
+
+//Bucle while
+
+
+let i = 0;
+while (i < miLista.length) {
+  console.log(miLista[i]);
+  i++;
+}
+
+//funcion flecha
+const saludar = () => 'Hola mundo';
+  console.log(saludar());
